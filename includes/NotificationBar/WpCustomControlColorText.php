f5c0ccfce8bbf825487e3a97ae434d7d6039023f
pnTrungPham <trung.nhatrangart@gmail.com>
2020-09-29 16:09:13 +0700

    Fix css select color

--- includes/NotificationBar/WpCustomControlColorText.php
+++ includes/NotificationBar/WpCustomControlColorText.php
@@ -13,11 +13,11 @@ defined('ABSPATH') || exit;
             <span class="customize-control-title"><?php echo esc_html( $this->label ); ?></span>
               <span id="nj_color_select_text">
                   <div class="wp-picker-container wp-picker-active nj_color_select_picker">
-                    <button type="button" class="button wp-color-result wp-picker-open nj_color_button_select_bg" aria-expanded="true" style="<?php echo esc_html('background-color:'.$this->value()); ?>"><span class="wp-color-result-text"><?php echo _e('Select Color', NJT_NOFI_DOMAIN);?></span></button>
+                    <button type="button" class="button nj_color_button_select_bg" aria-expanded="true" style="<?php echo esc_html('background-color:'.$this->value()); ?>"><span class="nj-color-result-text"><?php echo _e('Select Color', NJT_NOFI_DOMAIN);?></span></button>
                     <span class="wp-picker-input-wrap nj_color_display_picker njt_nofi_none">
                         <label>
                             <span class="screen-reader-text"><?php echo esc_html( $this->label ); ?></span>
-                            <input id="_customize-input-njt_nofi_text_color" class="njt_nofi_dp_none color-picker-hex wp-color-picker" type="text"  value="<?php echo esc_html($this->value()); ?>" data-default="<?php echo esc_html($this->value()); ?>" data-customize-setting-link="njt_nofi_text_color">
+                            <input id="_customize-input-njt_nofi_text_color" class="njt_nofi_dp_none nj-color-wp-color-picker" type="text"  value="<?php echo esc_html($this->value()); ?>" data-default="<?php echo esc_html($this->value()); ?>" data-customize-setting-link="njt_nofi_text_color">
                           </label>
                         <input type="button" class="button button-small wp-picker-default nj_color_button_select_default" value="<?php echo _e('Default', NJT_NOFI_DOMAIN);?>" aria-label="Select default color" />
                     </span>
